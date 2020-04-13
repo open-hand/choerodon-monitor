96d575a8a7c9b1d47cd71e0126d1c6bbd1e0c1ba
scp <changping.shi@hand-china.com>
2020-04-13 14:52:35 +0800

    [ADD] 初始化服务

---
+++ src/main/java/io/choerodon/monitor/MonitorApplication.java
@@ -0,0 +1,17 @@
+package io.choerodon.monitor;
+
+import org.hzero.autoconfigure.monitor.EnableHZeroMonitor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableHZeroMonitor
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/choerodon/monitor/HzeroMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.choerodon.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzeroMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
